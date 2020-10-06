76cf37c6e5d5c84541ab0b5bab8be5edd298c86f
abinayak4 <71189025+abinayak4@users.noreply.github.com>
2020-10-05 19:26:08 -0500

    Update Calculate Grades.java

--- Calculate Grades.java
+++ Calculate Grades.java
@@ -1,19 +1,33 @@
-package fib;
+public class Assign2 {
+	public static void main(String args[]) 
+	{
+	double grade1;
+	double grade2;
+	double grade3;
+	double grade4;
+	double grade5;
+	int avg_grade;
+	char Grade =0;
+	
+	grade1 = 91.14;
+	grade2 = 85.01;
+	grade3 = 72;
+	grade4 = 63;
+	grade5 = 81.14;
 
-public class Fibonacci {
+	avg_grade = (int)((grade1 + grade2 + grade3 + grade4 + grade5)/5);
+	
+	if( avg_grade < 100 && avg_grade>=90 ) {Grade = 'A';}
+	if( avg_grade < 90 && avg_grade>=80 ) {Grade = 'B';}
+	if( avg_grade < 80 && avg_grade>=70 ) {Grade = 'C';}
+	if( avg_grade < 70 ) {Grade = 'F';}
 
-	public static void main(String[] args) {
-		int n1 = 0, n2 = 1, n3, i, count = 10;
-		System.out.print(n1 + " " + n2);// printing 0 and 1
-
-		for (i = 2; i < count; ++i)// loop starts from 2 because 0 and 1 are already printed
-		{
-			n3 = n1 + n2;
-			System.out.print(" " + n3);
-			n1 = n2;
-			n2 = n3;
-		}
-
+	switch(Grade) {
+	case 'A' :System.out.println("A: Fantastic job!");break;
+	case 'B' :System.out.println("B: Good!");break;
+	case 'C' :System.out.println("C: Please try more next time!");break; 
+	case 'F' :System.out.println("F: Unfortunately you failed!");break;
+	default  :System.out.println("Invalid grade");
+	}	
 	}
-
 }
